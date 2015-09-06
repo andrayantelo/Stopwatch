4762bfa57a4745cd70e3490896ed2250a1c5562d
Andrea Anaya <andrayantelo@gmail.com>
2015-09-06 16:22:27 -0500

    fixed pause method, working on tests now

--- stopwatch/stopwatch.py
+++ stopwatch/stopwatch.py
@@ -16,6 +16,9 @@ class Stopwatch(object):
         
         self.pause_state = False
         self.time_of_pause = 0
+        self.stop_time = 0
+        self.pause_duration = 0
+        self.elapsed_time = 0
         
     def __repr__(self):
         return 'stopwatch()'
@@ -28,8 +31,9 @@ class Stopwatch(object):
     
     def _stop_timer(self):
         """ Stops the timer. Returns time elapsed."""
-        
-        self.stop_time = time.time()
+        print("pause duration")
+        print self.format_time(self.pause_duration)
+        self.stop_time = time.time() - self.pause_duration
         
         return self.stop_time - self.start_time
 
@@ -46,7 +50,7 @@ class Stopwatch(object):
         #milliseconds = clock_time - int(clock_time)  #does not work
         #print milliseconds
         minutes, sec = divmod(clock_time, 60)
-        output = "%02d:%02" % (minutes, sec)
+        output = "%02d:%02d" % (minutes, sec)
         return output
         
     def elapsed(self):
@@ -64,11 +68,15 @@ class Stopwatch(object):
         
         if not self.pause_state:
             self.time_of_pause = time.time()
+            print("time of pause")
+            print(self.time_of_pause)
             self.pause_state = True
-        if self.pause_state:
-            pause_duration = time.time() - self.time_of_pause
-            self.start_time = self.start_time + pause_duration
+            print(self.pause_state)
+        elif self.pause_state:
+            self.pause_duration = time.time() - self.time_of_pause
             self.pause_state = False
+            print self.format_time(self.pause_duration)
+            return self.pause_duration
             
         
 
