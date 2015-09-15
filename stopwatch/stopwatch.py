7189e34ca5dee95dae9e249d4c3576bb9c2985db
Andrea <andrayantelo@gmail.com>
2015-09-15 15:08:48 -0500

    got rid of pause method, modified stop_timer, made start time and stop time attributes private, made their methods public, changed convert time to include hours and milliseconds

--- stopwatch/stopwatch.py
+++ stopwatch/stopwatch.py
@@ -6,41 +6,42 @@ class Stopwatch(object):
         """ Create a stopwatch timer
         Parameters:"""
         
-        self.pause_state = False
-        self.time_of_pause = 0
-        self.stop_time = 0
-        self.pause_duration = 0
+        self._stop_time = 0
+        self._start_time = 0
         self.elapsed_time = 0
         
     def __repr__(self):
         return 'Stopwatch()'
         
-    def _start_timer(self):
+    def start_timer(self):
         """Starts the timer."""
         
-        self.start_time = time.time()
-        return self.start_time
+        self._start_time = time.time()
+        return 
     
-    def _stop_timer(self):
+    def stop_timer(self):
         """ Stops the timer. Returns time of stop."""
-        self.stop_time = time.time()
-        return self.stop_time
+        self._stop_time = time.time()
+        self.elapsed_time += (self._stop_time - self._start_time)
+        return self.elapsed_time
 
     def convert_time(self, clock_time):
-        """ Returns a tuple containing the minutes and seconds of a time given 
-        in seconds.
+        """ Returns a tuple containing the hours, minutes, seconds and
+        milliseconds of a time given in seconds.
         
         Parameters:
         clock_time: int
             the time, in seconds, to be converted.
         """
         
-        
-        clock_time = int(clock_time)
-        #milliseconds = clock_time - int(clock_time)  #does not work
-        #print milliseconds
-        minutes, sec = divmod(clock_time, 60)
-        output = (minutes, sec)
+        clock_time = clock_time
+        milliseconds = clock_time * 1000 
+        sec, milliseconds = divmod(round(milliseconds,2), 1000)
+        print sec
+        minutes, sec = divmod(round(clock_time,2), 60)
+        print sec
+        hours, minutes = divmod(round(minutes,2), 60)
+        output = (hours, minutes, sec, milliseconds)
         return output
         
     def format_time(self, clock_tuple):
@@ -51,6 +52,7 @@ class Stopwatch(object):
             tuple containing (minutes, seconds).
         """
         formatted_time = "%02d:%02d" %(clock_tuple[0], clock_tuple[1])
+        return formatted_time
         
     def elapsed(self):
         """ Returns the formatted time that has elapsed.
@@ -58,28 +60,12 @@ class Stopwatch(object):
         print("pause duration")
         print self.pause_duration
         self.elapsed_time = time.time() - self.pause_duration
-        return self.elapsed_time - self.start_time
+        return self.elapsed_time - self._start_time
         
     def reset(self):
         """ Resets the start time to 0. """
-        self.start_time = time.time()
-        return self.start_time
-        
-    def pause(self):
-        """ Pauses the stopwatch."""
-        
-        if not self.pause_state:
-            self.time_of_pause = time.time()
-            print("time of pause")
-            print(self.time_of_pause)
-            self.pause_state = True
-            print(self.pause_state)
-        elif self.pause_state:
-            self.pause_duration = time.time() - self.time_of_pause
-            self.pause_state = False
-            print self.format_time(self.pause_duration)
-            return self.pause_duration
-            
+        self._start_time = time.time()
+        return self._start_time
         
 
         
--- tests/stopwatch_tests.py
+++ tests/stopwatch_tests.py
@@ -6,6 +6,7 @@ class TestStopwatch(unittest.TestCase):
     
     def setUp(self):
         self.mytimer = Stopwatch()
+        self.epsilon = 0.1
     
     def test_repr(self):
         assert_equal(self.mytimer.__repr__(), "Stopwatch()")
@@ -14,27 +15,36 @@ class TestStopwatch(unittest.TestCase):
         mytimer = self.mytimer
         assert_equal(mytimer.pause_state , False)
         assert_equal(mytimer.time_of_pause, 0)
-        assert_equal(mytimer.stop_time, 0)
+        assert_equal(mytimer._stop_time, 0)
         assert_equal(mytimer.pause_duration, 0)
         assert_equal(mytimer.elapsed_time, 0)
+        assert_equal(mytimer._start_time, 0)
         
     def test_start_timer(self):
-        then = self.mytimer._start_timer()
-        time.sleep(1)
+        then = self.mytimer.start_timer()
         now = time.time()
         time_diff = now - then
-        assert_equal(round(time_diff, 2), 1)
+        assert_true(abs(time_diff < self.epsilon))
         
     def test_stop_timer(self):
-        start = mytimer._start_timer()
-        time.sleep(1)
-        stop = mytimer._stop_timer()
-        time_diff = stop - start
-        epsilon = 0.1
-        assert_equal(time_diff - 1 < epsilon, True)
+        start = mytimer.start_timer()
+        stop = mytimer.stop_timer()
+        assert_true(mytimer.elapsed_time < self.epsilon)
         
     def test_convert_time(self):
-        pass
+        time_diff = 5410.002 #seconds
+        time_diff = mytimer.convert_time(time_diff)
+        assert_equal(time_diff, (1.0, 30.0, 10.0, 2.0 ))
+        
+    def test_format_time(self):
+        time_min_sec = (55, 45)
+        mytimer.format_time(time_min_sec)
+        assert_equal(mytimer.format_time(time_min_sec), "55:45")
+        
+    def test_pause(self):
+        assert_equal(mytimer.pause_state, False)
+        
+        
         
         
 
