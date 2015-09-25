e9598713f19e77c4b755d3afd0c8ff8e360fd1ef
Andrea <andrayantelo@gmail.com>
2015-09-25 16:21:22 -0500

    attempting to use grid for my gui instead of pack

--- stopwatch/app.py
+++ stopwatch/app.py
@@ -9,24 +9,31 @@ class App():
         self.root.title("Stopwatch")
         
         self.on_state = True
+        
         self.textvar = StringVar()
         self.output = "00:00:00"
         self.textvar.set(self.output)
-        self.label = Label(textvariable=self.textvar, font=16)
-        self.label.pack(side=TOP)
+        self.label = Label(textvariable=self.textvar, font=("Arial",16)).grid(row=1,column=1)
+        #self.label.pack()
+        
+        self.milli_text = StringVar()
+        self.milli = "000"
+        self.milli_text.set(self.milli)
+        self.milliseconds = Label(textvariable=self.milli_text, font=("Arial",8)).grid(row=1, column=2)
+        #self.milliseconds.pack()
         
         frame = Frame(self.root)
-        frame.pack()
+        frame.grid(row=2)
         
         self.start_button = Button(frame, text="START", fg="green", command=self.start)
-        self.start_button.pack(side=LEFT)
+        self.start_button.grid(row=2, column=1)
         self.stop_button = Button(frame, text="STOP", fg="red", command=self.stop)
-        self.stop_button.pack(side=LEFT)
-        self.reset_button = Button(frame, text="RESET", fg="yellow", command=mytimer.reset)
-        self.reset_button.pack(side=LEFT)
+        self.stop_button.grid(row=2, column=2)
+        self.reset_button = Button(frame, text="RESET", fg="yellow", command=self.reset)
+        self.reset_button.grid(row=2, column=3)
         
         self.display_button = Button(frame, text="display", fg="blue", command=self.print_elapsed)
-        self.display_button.pack(side=LEFT)
+        self.display_button.grid(row=2, column=4)
         
         
         
@@ -34,8 +41,9 @@ class App():
     def print_elapsed(self):
         if self.on_state == True: 
             self.output = mytimer.format_time(mytimer.convert_time(mytimer.elapsed()))
+            self.output = self.output[0]
             self.textvar.set(self.output)
-            self.root.after(1000, self.print_elapsed)
+            self.root.after(60, self.print_elapsed)
         elif self.on_state == False:
             self.textvar.set(self.output)
         
@@ -49,7 +57,9 @@ class App():
         self.print_elapsed()
         
     def reset(self):
-        pass
+        self.on_state = False
+        self.output = "00:00:00"
+        self.print_elapsed()
         
         
     
--- stopwatch/stopwatch.py
+++ stopwatch/stopwatch.py
@@ -53,7 +53,8 @@ class Stopwatch(object):
             tuple containing (minutes, seconds).
         """
         formatted_time = "%02d:%02d:%02d" %(clock_tuple[0], clock_tuple[1], clock_tuple[2])
-        return formatted_time
+        formatted_milliseconds = "%03d" %(clock_tuple)[3]
+        return (formatted_time, formatted_milliseconds)
         
     def elapsed(self):
         """ Returns the time that has elapsed between starting the timer
