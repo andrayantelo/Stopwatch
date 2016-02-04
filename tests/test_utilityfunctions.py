b689d387764efa9c22415404db7e97548a6425aa
andrayantelo <andrayantelo@gmail.com>
2016-02-04 17:40:55 -0600

    working on test for hide_me function

--- tests/test_utilityfunctions.py
+++ tests/test_utilityfunctions.py
@@ -2,6 +2,8 @@
 import nose.tools as nt
 import unittest
 import stopwatch.utilityfunctions as uf
+import Tkinter as tk
+import cdapp as ca
 
 class TestUtilityfunctions(unittest.TestCase):
     
@@ -9,22 +11,38 @@ class TestUtilityfunctions(unittest.TestCase):
         pass
         
     def test_seconds_to_tuple(self):
-        time_diff = 5410 #seconds
-        time_diff = self.mytimer.convert_time(time_diff)
+        time_diff = 5410.001 #seconds
+        time_diff = uf.seconds_to_tuple(time_diff)
         
-        nt.assert_equal(time_diff, (1.0, 30.0, 10.0))
+        nt.assert_equal(time_diff, (1.0, 30.0, 10.0, 1))
         
         
     def test_tuple_to_seconds(self):
-        time_diff = (1.0, 30.0, 10.0)
-        time_diff = self.mytimer.revert_time(time_diff)
+        time_diff = (1.0, 30.0, 10.0, 1)
+        time_diff = uf.tuple_to_seconds(time_diff)
         
-        nt.assert_equal(time_diff, 5410)
-        nt.assert_raises(TypeError, self.mytimer.revert_time, 15)
+        nt.assert_equal(time_diff, 5410.001)
+        nt.assert_raises(TypeError, uf.tuple_to_seconds, 15)
         
     def test_tuple_to_clockface(self):
-        time_min_sec = (0, 55, 45, 0)
+        time_min_sec = (0, 55, 45, 1)
         
-        self.mytimer.format_time(time_min_sec)
+        uf.tuple_to_clockface(time_min_sec)
         
-        nt.assert_equal(self.mytimer.format_time(time_min_sec), ("00:55:45", "000"))
+        nt.assert_equal(uf.tuple_to_clockface(time_min_sec), ("00:55:45", "001"))
+        
+    def test_hide_me(self):
+        root = tk.Tk()
+        frame = tk.Frame(root)
+        test_button = tk.Button(frame)
+        test_button.pack()
+        
+        """ Can use the following:
+        root = tk.Tk()
+        w = Label(root, text="hello")
+        w.pack()
+        w.winfo_ismapped()
+        w.pack_forget()
+        w.winfo_ismapped() """
+        
+        
