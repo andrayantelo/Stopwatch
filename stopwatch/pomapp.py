5a0091a9092da90cbf3b23eb9998a8b7b95a0e08
andrayantelo <andrayantelo@gmail.com>
2016-02-12 15:02:28 -0600

    added pomodoro app module

---
+++ stopwatch/pomapp.py
@@ -0,0 +1,14 @@
+#pomodoro gui
+
+import countdown as cd
+import stopwatch as sw
+import time
+import Tkinter as tk
+from functools import partial
+from timeit import default_timer as real_time
+import utilityfunctions as uf
+
+class Pomapp(object):
+    
+    def __init__(self):
+        pass
--- tests/test_pomodoro.py
+++ tests/test_pomodoro.py
@@ -4,3 +4,33 @@ import nose.tools as nt
 import stopwatch.stopwatch as sw
 import unittest
 import countdown as ct
+
+class TestPomodoro(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+        
+    def test_init(self):
+        pass
+        
+    def test_advance_round(self):
+        pass
+        
+    def test_active_countdown(self):
+        pass
+        
+    def test_start_pomodoro(self):
+        pass
+        
+    def test_stop_pomodoro(self):
+        pass
+        
+    def test_time_remaining(self):
+        pass
+        
+    def test_reset_pomodoro(self):
+        pass
+        
+    def test_input_times(self):
+        pass
+    
